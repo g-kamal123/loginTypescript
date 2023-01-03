2627298e8ce3b6c92966d453558b381c778ceb55
g-kamal123 <kamalnayanrai@cedcoss.com>
2023-01-03 13:40:03 +0530

    corrected listing

--- src/utils/fetchHook/FetchHook.ts
+++ src/utils/fetchHook/FetchHook.ts
@@ -1,3 +1,5 @@
+import { token } from "../headers/Headers";
+
 export const useFetch = (url:any) => {
     const _get = async (endpoint:any={}, allParams:any) => {
         let url1 = url
@@ -22,7 +24,7 @@ export const useFetch = (url:any) => {
           Accept: "application/json",
           "Content-Type": "application/json",
           appTag: "twitter_ads",
-          Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VyX2lkIjoiNjM2MzcyZDgxODZlNjUzOWVkMDU5NmMyIiwicm9sZSI6ImN1c3RvbWVyIiwiZXhwIjoxNjcyMDQxNDg0LCJpc3MiOiJodHRwczpcL1wvYXBwcy5jZWRjb21tZXJjZS5jb20iLCJ0b2tlbl9pZCI6IjYzYTkxYmNjMzIxMzE3NTIxYTU4YzgwMiJ9.T_kndGEEentY5MLfpJsL4l29UF_EK4e9p_oWUcg4Dt2w7dTSHs72w3CVqsKD4bNQYenaOGDuS7LeMiosZiL2vfjoUT8VorenbzTTqLh9GTG55SUnDCRG41QaUCwoJwGAChSObDqyFRK51Y-UASyMjKu4hk2hRKzJe54Wx3c3QigmD_lUd9rwxJs0ma7LrHj3ARCDaUwIagsOXoBZqC4jn3L3WQMaSDh9w6x9Ka9R1X-3zrvyjt36931qWzCkTfgGBTz0N0ZQZULsJDA-X8R0ixH4iVEqkYuylKjqJWq_KXNN6paewDZFvkRW-WzRmHCQWzqY3PmlQMgAKAJVQR-pdw`,
+          Authorization: token,
           "Ced-Source-Id": "889",
           "Ced-Source-Name": "shopify",
           "Ced-Target-Id": "890",
--- src/utils/headers/Headers.ts
+++ src/utils/headers/Headers.ts
@@ -1,10 +1,11 @@
+export const token = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VyX2lkIjoiNjM2MzcyZDgxODZlNjUzOWVkMDU5NmMyIiwicm9sZSI6ImN1c3RvbWVyIiwiZXhwIjoxNjcyNzQwMzI0LCJpc3MiOiJodHRwczpcL1wvYXBwcy5jZWRjb21tZXJjZS5jb20iLCJ0b2tlbl9pZCI6IjYzYjNjNWE0OTQ1Mjg4NDEwNTcwMGE3MiJ9.lnZd-XcM2CSXDoszeeV_EbS_OT3_M3nKcP37Bc2PLqRf1V8Fc265X0TKYVe2CSun97FEaDBpjK-5b1PRxCckkMdfdpAAmMczktKgIKGw_x2zSKxZFqPXtKTpLRgBIcaWalBnqZhPZTAiA85xO__U9pF0DT0phwnpQuX0xUBhGz_ylcn7BhpAgk_-SejTXWgz2tD1PnS2RzSiAZ3IQgVlLARqgFD0S2QXZESTqkIuRV7Q_DNaFU96Y7y9lWmBN-dAoz3Wh1dE811qxKE-GC2V1PGJVWpQIjVpI9GsKLQrlF9lqWZseEKwlHYdnqP-WZpGWmKD55qeLf9lMNtOCoghOA`;
 export const getProductHeader = {
-    headers: {
-        appTag: "twitter_ads",
-        Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VyX2lkIjoiNjM2MzcyZDgxODZlNjUzOWVkMDU5NmMyIiwicm9sZSI6ImN1c3RvbWVyIiwiZXhwIjoxNjcyMDQxNDg0LCJpc3MiOiJodHRwczpcL1wvYXBwcy5jZWRjb21tZXJjZS5jb20iLCJ0b2tlbl9pZCI6IjYzYTkxYmNjMzIxMzE3NTIxYTU4YzgwMiJ9.T_kndGEEentY5MLfpJsL4l29UF_EK4e9p_oWUcg4Dt2w7dTSHs72w3CVqsKD4bNQYenaOGDuS7LeMiosZiL2vfjoUT8VorenbzTTqLh9GTG55SUnDCRG41QaUCwoJwGAChSObDqyFRK51Y-UASyMjKu4hk2hRKzJe54Wx3c3QigmD_lUd9rwxJs0ma7LrHj3ARCDaUwIagsOXoBZqC4jn3L3WQMaSDh9w6x9Ka9R1X-3zrvyjt36931qWzCkTfgGBTz0N0ZQZULsJDA-X8R0ixH4iVEqkYuylKjqJWq_KXNN6paewDZFvkRW-WzRmHCQWzqY3PmlQMgAKAJVQR-pdw`,
-        "Ced-Source-Id": "889",
-        "Ced-Source-Name": "shopify",
-        "Ced-Target-Id": "890",
-        "Ced-Target-Name": "twitter",
-      }
-}
+  headers: {
+    appTag: "twitter_ads",
+    Authorization: token,
+    "Ced-Source-Id": "889",
+    "Ced-Source-Name": "shopify",
+    "Ced-Target-Id": "890",
+    "Ced-Target-Name": "twitter",
+  },
+};
